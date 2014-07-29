b0c1051b969b3d645718d15d6461f66a5ef2a668
Julien Castelain <julienc@mbp.local>
2014-07-29 22:19:48 +0200

    playing around with gzip + organization

---
+++ test/snippet_test.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var assert = require('assert')
+  , snippet = require('../lib/snippet');
+
+describe('open', function () {
+
+  it('should generate a string', function () {
+    var str = snippet.generate('8888');
+
+    assert.notDeepEqual(str, undefined);
+    assert.notDeepEqual(str.length, 0);
+
+    assert.notDeepEqual(str.indexOf('8888'), -1);
+    assert.notDeepEqual(str.match(/\b8888\b/), null);
+
+  });
+
+}); 
